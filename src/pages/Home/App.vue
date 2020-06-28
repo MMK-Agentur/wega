ae4059401977773bb1692c14560c822462747e11
dantedubon <dante.dubon@gmail.com>
2020-06-28 16:14:24 -0600

    adding

--- src/pages/Home/App.vue
+++ src/pages/Home/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-  
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="bg-darkest">
+        <div class="page-container" style="min-height: auto;">
+            <div class="title-container--light">
+                <h3 class="title">Kontakt</h3>
+                <div class="row mt-5">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                        <div class="contact-info">
+                            <h6>WEGA Invest GmbH
+                            </h6>
+                            <p>Sporerstraße 3<br>
+                                80333 München
+                                </p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                        <form>
+                            <div class="form-group ">
+                                <textarea class="form-control form-contact" id="exampleFormControlTextarea1" rows="3" placeholder="Ihre Anfrage"></textarea>
+                              </div>
+                              <button type="button" class="btn btn-lightest">Senden</button>
+                        </form>
+                    </div>   
+                </div>
+                
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-import HelloWorld from '../../components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
